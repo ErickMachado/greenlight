f1a349c86c33cc02b8cba46b53526d89700b5fca
Erick Machado <erick.gsmachado@gmail.com>
2026-04-19 21:57:04 -0300

    ✅ Chapter 4.4

--- internal/data/runtime.go
+++ internal/data/runtime.go
@@ -1,9 +1,13 @@
 package data
 
 import (
+	"errors"
 	"fmt"
 	"strconv"
+	"strings"
 )
+
+var ErrInvalidRuntimeFormat = errors.New("invalid runtime format")
 
 type Runtime int
 
@@ -12,4 +16,24 @@ func (r Runtime) MarshalJSON() ([]byte, error) {
 	quoted := strconv.Quote(value)
 
 	return []byte(quoted), nil
+}
+
+func (r *Runtime) UnmarshalJSON(val []byte) error {
+	unquoted, err := strconv.Unquote(string(val))
+	if err != nil {
+		return ErrInvalidRuntimeFormat
+	}
+	parts := strings.Split(unquoted, " ")
+	if len(parts) != 2 || parts[1] != "mins" {
+		return ErrInvalidRuntimeFormat
+	}
+
+	i, err := strconv.Atoi(parts[0])
+	if err != nil {
+		return ErrInvalidRuntimeFormat
+	}
+
+	*r = Runtime(i)
+
+	return nil
 }
