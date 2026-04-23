80bc5b882b2fd62a3ec35568e74bbf1872db6563
Erick Machado <erick.gsmachado@gmail.com>
2026-04-23 20:18:38 -0300

    ✅ Chapter 8.2

--- internal/data/movies.go
+++ internal/data/movies.go
@@ -100,7 +100,7 @@ func (m MovieModel) Update(movie *Movie) error {
 			"runtime" = $3,
 			"genres" = $4,
 			"version" = "version" + 1
-		WHERE "id" = $5
+		WHERE "id" = $5 AND "version" = $6
 		RETURNING "version"
 	`
 	args := []any{
@@ -109,9 +109,20 @@ func (m MovieModel) Update(movie *Movie) error {
 		movie.Runtime,
 		pq.Array(movie.Genres),
 		movie.ID,
+		movie.Version,
 	}
 
-	return m.DB.QueryRow(stmt, args...).Scan(&movie.Version)
+	err := m.DB.QueryRow(stmt, args...).Scan(&movie.Version)
+	if err != nil {
+		switch {
+		case errors.Is(err, sql.ErrNoRows):
+			return ErrEditConflict
+		default:
+			return err
+		}
+	}
+
+	return nil
 }
 
 func (m MovieModel) Delete(id int) error {
