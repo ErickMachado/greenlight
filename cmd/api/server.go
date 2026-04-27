9401ab9357c7db3436a6541741aa7065f8ce0d13
Erick Machado <erick.gsmachado@gmail.com>
2026-04-27 11:11:33 -0300

    ✅ Chapter 11.3

--- cmd/api/server.go
+++ cmd/api/server.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"context"
+	"errors"
 	"fmt"
 	"log/slog"
 	"net/http"
@@ -19,13 +21,31 @@ func (app *application) serve() error {
 		WriteTimeout: 10 * time.Second,
 		ErrorLog:     slog.NewLogLogger(app.logger.Handler(), slog.LevelError),
 	}
+
+	shutdownErr := make(chan error)
 	go func() {
 		quit := make(chan os.Signal, 1)
 		signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
 		s := <-quit
-		app.logger.Info("caught signal", "signal", s)
-		os.Exit(0)
+		app.logger.Info("shutting down server", "signal", s.String())
+		ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+		defer cancel()
+		shutdownErr <- srv.Shutdown(ctx)
 	}()
+
 	app.logger.Info("starting server", "addr", srv.Addr, "env", app.config.env)
-	return srv.ListenAndServe()
+
+	err := srv.ListenAndServe()
+	if !errors.Is(err, http.ErrServerClosed) {
+		return err
+	}
+
+	err = <-shutdownErr
+	if err != nil {
+		return err
+	}
+
+	app.logger.Info("stopped server", "addr", srv.Addr)
+
+	return nil
 }
